df6f85c70b6e640b13c4c2d38c8612bf5c9c4001
motoshi <localmost@yoneshukishi-no-MacBook-Pro.local>
2014-11-25 19:42:45 +0900

    Initial Commit

---
+++ callObc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  callObc
+//
+//  Created by motoshi on 2014/11/25.
+//  Copyright (c) 2014年 motoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
